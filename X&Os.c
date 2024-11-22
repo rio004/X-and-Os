fd2cb13b0030d6ae1105c9faf40fd97adc6363cd
Mahmoud-m-k <mahmoudkhater06@gmail.com>
2024-11-22 12:11:54 +0000

    Update X&Os.c
    styling still needs to be added

--- X&Os.c
+++ X&Os.c
@@ -1,8 +1,29 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main(void)
+char board[10]={' ','1','2','3','4','5','6','7','8','9'};
+
+void drawTable()
 {
-    printf("Hello world");
+    int i;
+    for(i = 0; i <= 9; i++)
+    {
+        if(board[i]==' ')
+        {
+            continue;
+        }
+        if(i==4|i==7)
+        {
+            printf("\n");
+        }
+        printf("%c",board[i]);
+        
+    }
+    //Styling still needs to be added.
+}
+
+int main()
+{
+    drawTable();
 
     return 0;
 }
