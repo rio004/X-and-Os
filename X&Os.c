c5486a3cc4951bf4296feb023db6565a2cb25176
Mahmoud-m-k <mahmoudkhater06@gmail.com>
2024-11-22 13:09:42 +0000

    MahmoudsGrid
    Change-Id: Iaeca02bd6a51d8ea2a890264b08a137145799acc

--- X&Os.c
+++ X&Os.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
 
+char board[10]= {' ','1','2','3','4','5','6','7','8','9'};
+
 void drawTable()
 {
+    int i;
+
+    for(i = 0; i <= 9; i++)
+    {
+        if(board[i]==' ')
+        {
+            continue;
+        }
+
+        if(i==4|i==7)
+        {
+            printf("\n");
+            printf(" -------------------");
+        }
+        if(i==4|i==7)
+        {
+            printf("\n");
+        }
+        if(i == 1|i==4|i==7)
+        {
+            printf(" | ");
+        }
+
+        printf(" %c ",board[i]);
+        printf(" | ");
+    }
+    printf("\n");
+    printf("\n");
 }
 
 void getMove(char player)
