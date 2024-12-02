988a29334677b6af0d86025b4283fbd59de65f16
rio004 <riyon336@gmail.com>
2024-12-02 00:39:16 +0000

    Add some more features
    * Main menu
    * Draw checker
    * Random player
    * Computer

--- X&Os.c
+++ X&Os.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 char board[10]= {' ','1','2','3','4','5','6','7','8','9'};
 
@@ -90,23 +92,108 @@ int isWinner(char player)
 	return 0;
 }
 
+// Checks if the board is full
+int fullBoard() {
+    // loops through the board
+    for (int i = 1; i <= 9; i++) {
+        // Checks if X and O are not in the board
+        if (board[i] != 'X' && board[i] != 'O') {
+            return 0;       // the game continues
+        }
+    }
+    return 1;               // the game becomes a draw
+}
+
+void computerMove(char player) 
+{
+    int cell;
+    srand(time(NULL));
+
+	do {
+        cell = rand() % 9 + 1;
+        // Checks if X and O are not in the cell chosen
+        if (board[cell] != 'X' && board[cell] != 'O') {
+            // chooses a random number from 1 to 9 and plays for player
+            board[cell] = player;  
+            printf("Computer has chosen %d \n", cell);
+            return;
+        }
+    } while (1);
+}
+
 int main(void)
 {
+    // Main menu
+    int choice = 0, computer = 0, multiplayer = 0;
+
+    // Random player
+    int random;
+    int player, player2;
+
+    do { 
+        printf("\nMain Menu\n");
+        printf("\nMultiplayer: 1 \n");
+        printf("Computer: 2 \n");
+        scanf("%d", &choice);
+
+        if (choice < 1 || choice > 2) {
+            printf("\nChoose the correct option!!!\n");
+        } else {
+            if (choice == 1) {
+                multiplayer = 1;
+            } else {
+                computer = 1;
+            }
+            break;
+        }
+    } while(1);
+
+    // TODO
+    // Mahmoud: Multiplayer best of 3
+
+    // Initial game layout
     drawTable();
 
+    // Random player
+    srand(time(NULL));
+    random = rand() % 2;
+
+    if (random == 0) {
+        player = 'X';
+        player2 = 'O';
+    } else {
+        player = 'O';
+        player2 = 'X';
+    }
+
+    // Loops until the game is over
     while (1)
     {
-        getMove('X');
-        drawTable();
-        if (isWinner('X')) {
-            printf("\n X has won the game!!");
+        // player 1
+        getMove(player);           // 1st player is played
+        drawTable();               // redraws the board with player
+        if (isWinner(player)) {    // checks if X has won the game 
+            printf("\n %c has won the game!!", player);
+            break;
+        }
+        if (fullBoard()) {         // checks if the board is full
+            printf("\n Game is a draw!!");
             break;
         }
 
-        getMove('O');
+        // player 2 (could also be computer)
+        if (computer) {
+            computerMove(player2);
+        } else {
+            getMove(player2);
+        }
         drawTable();
-        if (isWinner('O')) {
-            printf("\n O has won the game!!");
+        if (isWinner(player2)) {
+            printf("\n %c has won the game!!", player2);
+            break;
+        }
+        if (fullBoard()) {
+            printf("\n Game is a draw");
             break;
         }
     }
